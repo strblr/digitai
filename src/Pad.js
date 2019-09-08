795cac5f632695fbd40465e144f1379e93c572e0
ostrebler <strebler.olivier@hotmail.fr>
2019-09-08 03:15:37 +0200

    Progress made

--- src/Pad.js
+++ src/Pad.js
@@ -1,36 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
 import cn from 'classnames'
 
-export default ({ pixmap, setPixmap }) => {
-  const [on, setOn] = useState(false)
-  return (
-    <div
-      id='pad'
-      onPointerDown={event => {
-        event.preventDefault()
-        setOn(true)
-      }}
-      onPointerUp={() => setOn(false)}>
-        {pixmap.map((row, i) => (
-          <div key={i}>
-          {row.map((pix, j) => (
-            <div
-              key={j}
-              className={cn({ active: pix })}
-              onMouseEnter={event => {
-                if(on) {
-                  if(event.nativeEvent.which === 0)
-                    setOn(false)
-                  else {
-                    pixmap[i][j] = true
-                    setPixmap([...pixmap])
-                  }
-                }
-              }}
-            />
-          ))}
-          </div>
+export default ({ pixmap, setPixmap }) => (
+  <div
+    id='pad'
+    onPointerDown={event => {
+      event.preventDefault()
+    }}>
+      {pixmap.map((row, i) => (
+        <div key={i}>
+        {row.map((pix, j) => (
+          <div
+            key={j}
+            className={cn({ active: pix })}
+            onMouseEnter={event => {
+              if(event.nativeEvent.which === 1) {
+                pixmap[i][j] = true
+                setPixmap([...pixmap])
+              }
+            }}
+          />
         ))}
-    </div>
-  )
-}
+        </div>
+      ))}
+  </div>
+)
