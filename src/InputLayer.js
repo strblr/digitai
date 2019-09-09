24ccc89649def0a23469ea3e39dc69556f711329
ostrebler <strebler.olivier@hotmail.fr>
2019-09-09 04:08:51 +0200

    Progress made

---
+++ src/InputLayer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from 'antd'
+
+export default () => (
+  <div>
+    <Button>salut</Button>
+  </div>
+)
--- src/Result.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default ({ result }) => (
-  <div id='result'>
-    {result}
-  </div>
-)
